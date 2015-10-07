3898dcb009e85364af237e1eb46ce92c0b2f4dee
yobeewon <moeberry06@gmail.com>
2015-10-07 07:35:52 -0400

    basic console log and document.write JS

---
+++ javaScriptFun/script.js
@@ -0,0 +1,5 @@
+console.log("Program start");
+alert("Hello JS");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>Here I will be practicing debugging methods for your pleasure.</p>");
+console.log("Program end");
